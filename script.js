0125a6b02b96047739767bf90c14cefdb64d9721
Edris <eeffathi@yahoo.com>
2020-08-17 01:20:28 +0430

    setting the clock function to run every second

--- script.js
+++ script.js
@@ -2,17 +2,21 @@ const HRARM = document.querySelector("#hour");
 const MINRARM = document.querySelector("#minute");
 const SECRARM = document.querySelector("#second");
 
-var nowDate = new Date();
-let hourValue = nowDate.getHours();
-let minValue = nowDate.getMinutes();
-let secValue = nowDate.getSeconds();
+function setTheClock () {
+    var nowDate = new Date();
+    let hourValue = nowDate.getHours();
+    let minValue = nowDate.getMinutes();
+    let secValue = nowDate.getSeconds();
 
-console.log(nowDate);
+    // console.log(nowDate);
 
-let hourDegValue = hourValue*360/12 + (minValue*360/60)/12;
-let minDegValue = minValue*360/60 + (secValue*360/60)/60;
-let secDegValue = secValue*360/60;
+    let hourDegValue = hourValue*360/12 + (minValue*360/60)/12;
+    let minDegValue = minValue*360/60 + (secValue*360/60)/60;
+    let secDegValue = secValue*360/60;
 
-HRARM.style.transform = "rotate(" + hourDegValue + "deg)";
-MINRARM.style.transform = "rotate(" + minDegValue + "deg)";
-SECRARM.style.transform = "rotate(" + secDegValue + "deg)";
+    HRARM.style.transform = "rotate(" + hourDegValue + "deg)";
+    MINRARM.style.transform = "rotate(" + minDegValue + "deg)";
+    SECRARM.style.transform = "rotate(" + secDegValue + "deg)";
+}
+
+var intervalClock = setInterval(setTheClock , 1000);
