30f6125149444a9f49ac761b47129bdd044b38d1
Edris <eeffathi@yahoo.com>
2020-08-17 01:47:50 +0430

    fixing the bug where every arrow reaches zero, it swings back instead of continuing its rotation

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ const HRARM = document.querySelector("#hour");
 const MINRARM = document.querySelector("#minute");
 const SECRARM = document.querySelector("#second");
 
-function setTheClock () {
-    var nowDate = new Date();
+var nowDate = new Date();
     let hourValue = nowDate.getHours();
     let minValue = nowDate.getMinutes();
     let secValue = nowDate.getSeconds();
@@ -14,9 +13,15 @@ function setTheClock () {
     let minDegValue = minValue*360/60 + (secValue*360/60)/60;
     let secDegValue = secValue*360/60;
 
+
+
+function setTheClock () {
+    hourDegValue+= 360/3600/12;
+    minDegValue+= 360/3600;
+    secDegValue+= 360/60;
+
     HRARM.style.transform = "rotate(" + hourDegValue + "deg)";
     MINRARM.style.transform = "rotate(" + minDegValue + "deg)";
     SECRARM.style.transform = "rotate(" + secDegValue + "deg)";
 }
-
 var intervalClock = setInterval(setTheClock , 1000);
