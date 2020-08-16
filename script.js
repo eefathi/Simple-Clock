b213c8357986354b0ff45c38df533cbe27cfc999
Edris <eeffathi@yahoo.com>
2020-08-17 01:13:33 +0430

    injecting the current date by JS

--- script.js
+++ script.js
@@ -2,9 +2,16 @@ const HRARM = document.querySelector("#hour");
 const MINRARM = document.querySelector("#minute");
 const SECRARM = document.querySelector("#second");
 
-let hourDegValue = 15;
-let minDegValue = 55;
-let secDegValue = 25;
+var nowDate = new Date();
+let hourValue = nowDate.getHours();
+let minValue = nowDate.getMinutes();
+let secValue = nowDate.getSeconds();
+
+console.log(nowDate);
+
+let hourDegValue = hourValue*360/12 + (minValue*360/60)/12;
+let minDegValue = minValue*360/60 + (secValue*360/60)/60;
+let secDegValue = secValue*360/60;
 
 HRARM.style.transform = "rotate(" + hourDegValue + "deg)";
 MINRARM.style.transform = "rotate(" + minDegValue + "deg)";
